7f64ade7a7873c4e76acbb9f3031175b1e407b95
collabvm-hub <54394247+collabvm-hub@users.noreply.github.com>
2019-08-22 04:18:32 -0400

    ahahahaah the best math library

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function math(num1, num2, operator) {
+	if(operator == '+' && num1 == '1' && num2 == '1') console.log('2');
+	if(operator == '+' && num1 == '2' && num2 == '2') console.log('4');
+	if(operator == '+' && num1 == '3' && num2 == '3') console.log('6');
+	if(operator == '+' && num1 == '4' && num2 == '4') console.log('8');
+	if(operator == '+' && num1 == '5' && num2 == '5') console.log('10');
+	if(operator == '+' && num1 == '6' && num2 == '6') console.log('12');
+	if(operator == '+' && num1 == '7' && num2 == '7') console.log('14');
+	if(operator == '+' && num1 == '8' && num2 == '8') console.log('16');
+	if(operator == '+' && num1 == '9' && num2 == '9') console.log('18');
+	if(operator == '+' && num1 == '10' && num2 == '10') console.log('20');
+	if(operator == '+' && num1 == '11' && num2 == '11') console.log('22');
+	if(operator == '+' && num1 == '12' && num2 == '12') console.log('24');
+	if(operator == '+' && num1 == '13' && num2 == '13') console.log('26');
+	if(operator == '+' && num1 == '14' && num2 == '14') console.log('28');
+	if(operator == '+' && num1 == '15' && num2 == '15') console.log('30');
+	if(operator == '+' && num1 == '16' && num2 == '16') console.log('32');
+}
